fb8ec84d9f66b034eabdf8be2e4efcc44af3e05c
zhangchunlei <15116991665@163.com>
2020-12-30 19:26:20 +0800

    加跳转

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,6 +1,11 @@
 <template>
   <view class="page">
     <image class="header" src="../../static/images/demo.jpg"></image>
+    <view class="content">
+      <navigator url="/c/demo/index">
+        <button type="primary">跳转到分包(/c/demo/index)</button>
+      </navigator>
+    </view>
   </view>
 </template>
 
